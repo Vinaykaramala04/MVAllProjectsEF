42fea24e6039f0140065ec32f629b796258a52d2
vinay karamala <karamalavinay2004@gmail.com>
2025-08-05 18:45:49 +0530

    Added CodeFirst project

---
+++ CodeFirstDay2/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstDay2.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2); // ✅ Sets decimal(18,2)
+        }
+    }
+}
---
+++ CodeFirstDay2/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeFirstDay2.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; } // PK
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set; } // PK
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+    }
+
+}
